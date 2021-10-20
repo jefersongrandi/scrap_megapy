aa9de5b9787db07bf49415bd60546926a9a19066
Jeferson <jeferson.grandi@i-value.com.br>
2021-10-20 13:48:42 -0300

    ajuste

--- api.py
+++ api.py
@@ -4,14 +4,16 @@ from json import dumps
 from src.scrap import getResultMegasenaScrapping
 
 app = Flask(__name__)
+port = int(os.environ.get("PORT", 5000)) #Heroku will set the PORT environment variable for web traffic
 
 @app.route("/", methods=['GET'])
 def working():
+    return 'Api working well'
+
+@app.route("/megasena", methods=['GET'])
+def getresult():
     res = getResultMegasenaScrapping()
     return dumps(res, indent=2)
 
-#app.run()
-
 if (__name__ == '__main__'):
-    port = os.environ.get("PORT", 5000) #Heroku will set the PORT environment variable for web traffic
     app.run(debug=False, host="0.0.0.0", port=port) #set debug=False before deployment
